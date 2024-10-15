66a031624fefd8941538445fab6b66d01fffedb2
akshayvijaynair <akshay.v.nair@pace.edu>
2024-10-15 19:11:00 -0400

    feat(tictactoe): added new page for tic tac toe game

--- week3/src/components/NavBar.tsx
+++ week3/src/components/NavBar.tsx
@@ -19,6 +19,9 @@ return(
                     <Nav.Link>
                         <Link to={"/counter"}>Counter</Link>
                     </Nav.Link>
+                    <Nav.Link>
+                        <Link to={"/tictactoe"}>TicTacToe</Link>
+                    </Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">
---
+++ week3/src/pages/TicTacToe.tsx
@@ -0,0 +1,17 @@
+import {useEffect, useState} from "react";
+import {Button, Col, Container, Row} from "react-bootstrap";
+
+
+export default function TicTacToe() {
+
+
+    return (
+        <Container className="mt-5">
+            <Row>
+                <Col>
+                    <h1 >Tic Tac Toe</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
