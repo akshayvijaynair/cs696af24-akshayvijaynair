a4663082f936511a9c4ef7d0c93d3ba0c9595d2a
akshayvijaynair <akshay.v.nair@pace.edu>
2024-10-15 19:23:29 -0400

    feat(Square): added square component for playing tiles

--- week3/src/pages/TicTacToe.tsx
+++ week3/src/pages/TicTacToe.tsx
@@ -1,15 +1,19 @@
-import {useEffect, useState} from "react";
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
+import Square from "../components/Square";
 
 
 export default function TicTacToe() {
-
 
     return (
         <Container className="mt-5">
             <Row>
                 <Col>
                     <h1 >Tic Tac Toe</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Square />
                 </Col>
             </Row>
         </Container>
