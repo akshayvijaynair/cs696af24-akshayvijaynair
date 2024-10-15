29d0abe4f657386af77400a2e49219e6e00d3cd8
akshayvijaynair <akshay.v.nair@pace.edu>
2024-10-15 19:33:47 -0400

    feat(Board): added state to board to track values

--- week3/src/components/Square.tsx
+++ week3/src/components/Square.tsx
@@ -1,7 +1,23 @@
+import {useState} from "react";
+
 interface SquareProps {
     value: string;
 }
 
-export default function Square({ value }:SquareProps) {
-    return <button className="square">{value}</button>;
+export default function Square() {
+    const [value, setValue] = useState("");
+
+    const handleClick = () => {
+        setValue('X');
+    }
+
+
+    return (
+        <button
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
 }
