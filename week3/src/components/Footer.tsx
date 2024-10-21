2b2df255244491c5ce0e63ce460078a249408034
akshayvijaynair <akshay.v.nair@pace.edu>
2024-10-21 09:57:01 -0400

    feat(Footer): Added footer for dashboard

---
+++ week3/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer () {
+    return (
+        <footer className=" p-3 bg-light text-center bottom-0 w-100">
+            &copy; 2024 Kalyug. All rights reserved.
+        </footer>
+    )
+}
