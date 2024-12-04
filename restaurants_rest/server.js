b802634939edb50f2400db8f1c0edc4d1b4906d2
akshayvijaynair <akshay.v.nair@pace.edu>
2024-12-03 22:43:34 -0500

    feat(Lab2): added support for redit caching in application

--- restaurants_rest/server.js
+++ restaurants_rest/server.js
@@ -1,18 +1,29 @@
 import express from "express";
 import cors from "cors";
 import db from "./db/connection.js";
+import redisClient from "./db/redis.js";
 const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get('/restaurants/:id', function(req, res) {
+app.get('/restaurants/:id', async function(req, res) {
     const restaurant_id = req.params["id"];
-    db.collection("restaurants").findOne({
-        restaurant_id: restaurant_id,
-    })
-        .then(value => res.send(value))
-        .catch((reason) => res.status(500).send(`Not Found ${reason}`));
+    const value = await redisClient.get(restaurant_id);
+    if (value) {
+        res.send({"cached": true, "value": JSON.parse(value)});
+    } else {
+        const dbValue = await db.collection("restaurants").findOne({
+            restaurant_id: restaurant_id,
+        });
+        if (dbValue) {
+            await redisClient.set(restaurant_id, JSON.stringify(dbValue));
+            res.send({"cached": false, "value": dbValue});
+        } else {
+            res.status(500).send("Not Found");
+        }
+    }
 });
+
 
 app.get('/restaurants', function (req, res) {
     const { borough, cuisine } = req.query;
@@ -50,15 +61,6 @@ app.get('/restaurants', function (req, res) {
         .catch(error => res.status(500).json({ error: `An error occurred: ${error.message}` }));
 });
 
-app.get('/restaurants/:id', function(req, res) {
-    const restaurant_id = req.params["id"];
-    db.collection("restaurants").findOne({
-        restaurant_id: restaurant_id,
-    })
-        .then(value => res.send(value))
-        .catch((reason) => res.status(500).send(`Not Found ${reason}`));
-});
-
 app.post('/restaurants', function(req, res) {
     const restaurant_id = req.body['restaurant_id'];
     const name = req.body['name'];
@@ -76,14 +78,19 @@ app.post('/restaurants', function(req, res) {
 });
 
 
-app.delete('/restaurants/:id', function(req, res) {
+app.delete('/restaurants/:id', async function(req, res) {
     const restaurant_id = req.params["id"];
+    const redisExists = await redisClient.get(restaurant_id);
+    if (redisExists) {
+        await redisClient.del(restaurant_id);
+        console.log(`Deleted restaurant with ID ${restaurant_id} from cache`);
+    }
+
     db.collection("restaurants").deleteOne({
         restaurant_id: restaurant_id,
-    }).then(result => result.acknowledged && result.deletedCount >= 1 ?
-        res.send("Success") :
-        res.status(500).send("Failed")
-    ).catch(() => res.status(500).send("Not Found"));
+    }).then((result) => {
+        result.acknowledged && result.deletedCount >= 1 ? res.send("Success"): res.status(500).send("Failed")
+        }).catch(() => res.status(500).send("Not Found"));
 });
 
 app.listen(PORT, () => {
