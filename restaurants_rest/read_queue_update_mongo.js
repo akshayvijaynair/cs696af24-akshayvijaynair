24f7b53b3de3ed2a56dba1c411fced8999bcc9cb
akshayvijaynair <akshay.v.nair@pace.edu>
2024-12-04 21:45:53 -0500

    feat(Lab3): added conditional block for queries to delete data

--- restaurants_rest/read_queue_update_mongo.js
+++ restaurants_rest/read_queue_update_mongo.js
@@ -4,19 +4,36 @@ import 'dotenv/config';
 
 const uri = process.env.MONGO_CONN;
 const client = new MongoClient(uri);
+
 async function run() {
     const messageQueue = new Queue('messageQueue');
+
     messageQueue.process(async (job) => {
         console.log('Processing : ' + job.id);
         const database = client.db('sample_restaurants');
         const restaurants = database.collection('restaurants');
         const query = { restaurant_id: job.data.restaurant_id };
-        await restaurants.updateOne(
-            query,
-            {$set: JSON.parse(job.data.updates)},
-            {upsert: true}
-        );
-        console.log('Processed: ' + JSON.stringify(job.data));
+
+        try {
+            if (JSON.parse(job.data.updates) === "delete") {
+                const deleteResult = await restaurants.deleteOne(query);
+                if (deleteResult.acknowledged && deleteResult.deletedCount > 0) {
+                    console.log(`Deleted restaurant_id: ${job.data.restaurant_id}`);
+                } else {
+                    console.log(`No document found: ${job.data.restaurant_id}`);
+                }
+            }  else {
+                await restaurants.updateOne(
+                    query,
+                    { $set: JSON.parse(job.data.updates) },
+                    { upsert: true }
+                );
+                console.log('Updated document: ' + JSON.stringify(job.data));
+            }
+        } catch (error) {
+            console.error(`Error processing job ${job.id}:`, error);
+        }
     });
 }
+
 run().catch(console.dir);
